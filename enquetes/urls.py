e317d975d6f3509c1158b33be81bfa5b6c34babe
NikcWill <nwtads03@gmail.com>
2023-12-05 20:59:02 -0300

    acounts configu

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
 
   path('', views.index, name='home'),
-  
+  path('add-pergunta/', views.add_pergunta, name='add-pergunta')
 
 ]
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, HttpResponse, redirect
 from datetime import datetime
+from.models import Pergunta, OpcaoResposta
 from django.contrib.auth.decorators import login_required
+from random import randint
 
 @login_required(redirect_field_name='login')
 def index(request):
 
   
     return render(request, 'pages/index.html')
+
+def add_pergunta(request ):
+  if request.method == 'POST':
+
+    titulo = request.POST.get('name')
+    pergunta = request.POST.get('email')
+    cod = randint(100, 10000)
+    data_criacao = datetime.now()
+    active = True
+
+    Pergunta.objects.create(
+      
+    )
+    return redirect('login')
+    
+  else:
+    return render(request,'pages/add-pergunta.html') 
