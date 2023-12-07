f42ac3afdc16f6dcd21a686c17b5535d4ce41236
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-12-06 23:30:50 -0300

    estilizando botoes e mudando posição

--- conf/urls.py
+++ conf/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('', include('enquetes.urls')),
     path('accounts/', include('accounts.urls'))
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
